57ad8df46f0994530154ed22b36bf180e51fbe24
weiernt <48479774+weiernt@users.noreply.github.com>
2020-04-26 15:58:19 +1000

    Add files via upload
    Added banner to main home page, and blog style posts, and sidebar for main home page.

--- main_site.py
+++ main_site.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 @app.route('/home')
@@ -14,4 +14,12 @@ def login():
 	
 	
 if __name__ == "__main__":
+	
 	app.run(debug=True)
+	
+	# below use for testing url_for locations
+	# with app.test_request_context():
+		# print(url_for('index'))
+		# print(url_for('login'))
+		# print(url_for('login', next='/'))
+		# print(url_for('profile', username='John Doe'))
